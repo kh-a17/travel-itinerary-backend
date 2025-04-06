9473b59afc4e7e9a0982dbc9ab6733e152c21112
Khushi Agrawal <khushiagrawal@Khushis-MacBook-Air-3.local>
2025-04-06 10:55:51 -0500

    fixed code error

--- backend/routes/postCreateRoutes.js
+++ backend/routes/postCreateRoutes.js
@@ -4,8 +4,5 @@ const { postCreate, getPostInfo } = require('../controllers/postCreator');
 
 router.post('/create-post', postCreate);
 router.get('/get-post-info', getPostInfo); // optional
-const { postCreate } = require('../controllers/postCreator');
-
-router.post('/create-post', postCreate);
 
 module.exports = router;
